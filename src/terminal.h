9d5ed293210cbdcd64f6467243e7488480e345af
Aidan <aidansocal844@gmail.com>
2023-02-19 15:30:23 -0800

    terminal sadness

--- src/terminal.h
+++ src/terminal.h
@@ -138,10 +138,10 @@ void Terminal::exit() {
     showCursor();
     setCursorPosition(0, 0);
 
-    printf("\033[?1049l");  // Disable alternate screen buffer
-    printf("\033[?1000l");  // Disable mouse input
-    oldTerminalSettings.c_lflag |= ICANON | ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldTerminalSettings);
+    // printf("\033[?1049l");  // Disable alternate screen buffer
+    // printf("\033[?1000l");  // Disable mouse input
+    // oldTerminalSettings.c_lflag |= ICANON | ECHO;
+    // tcsetattr(STDIN_FILENO, TCSANOW, &oldTerminalSettings);
 
     // for (int t = 5; t > 0; t--) {
     //     int time = t * t;
