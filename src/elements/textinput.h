53493ebddc7555de35aecef0a48ddb4be18b7db6
Aidan <aidansocal844@gmail.com>
2023-02-20 02:14:12 -0800

    health notification

--- src/elements/textinput.h
+++ src/elements/textinput.h
@@ -111,7 +111,6 @@ void TextInput::paint(int x, int y, int width, int height) {
     drawText("│", x + width, y + 1, state);
     drawText("╰", x, y + 2, state);
     drawText("╯", x + width, y + 2, state);
-    cout << flush;
 }
 
 void TextInput::keyEvent(int key) {
@@ -119,7 +118,7 @@ void TextInput::keyEvent(int key) {
         if (text.length() > 0) {
             text = text.substr(0, text.length() - 1);
         }
-    } else if (key != Key::ENTER) {
+    } else if (key == Key::ENTER) {
         if (exitAction != nullptr) {
             exitAction();
         }
