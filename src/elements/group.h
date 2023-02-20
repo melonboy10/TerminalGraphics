e6419cdee51a928e5ee22b060d72af5bee0fba0e
Aidan <aidansocal844@gmail.com>
2023-02-20 03:32:54 -0800

    bg

--- src/elements/group.h
+++ src/elements/group.h
@@ -109,8 +109,12 @@ void Group::paint(int x, int y, int width, int height) {
     WindowElement::paint(x, y, width, height);
     if (this->hidden) return;
     if (!this->borderHidden) {
-        fillArea(x, y, width, height, this->backgroundColor);
+        // set background color
+        if (backgroundColor != Color::RESET) {
+            drawText("", x, y, backgroundColor + 10);
+        }
         drawBox(x, y, width, height, title);
+        drawText("", x, y, 0);
         this->layout->paint(x + 1, y + 1, width - 2, height - 2, this->elements);
     } else {
         this->layout->paint(x, y, width, height, this->elements);
