1bf9aae35fd1b09d9a8ed4dbcadaf155f5972472
Aidan <aidansocal844@gmail.com>
2023-03-07 02:10:11 -0800

    cursor and line numbers

--- src/elements/textfield.h
+++ src/elements/textfield.h
@@ -40,6 +40,12 @@ class TextField : public WindowElement {
     vector<string> getText();
 
     /**
+     * Sets whether or not the text input should show line numbers
+     * @param lineNumbers whether or not the text input should show line numbers
+     */
+    void setLineNumbers(bool lineNumbers);
+
+    /**
      * Paints the text input on the screen
      * @param x the x position of the text input on the screen
      * @param y the y position of the text input on the screen
@@ -78,6 +84,7 @@ class TextField : public WindowElement {
     vector<string> text = vector<string>(1, "");
     pair<int, int> selectedText = pair<int, int>(0, 0);
     string templateText;
+    bool lineNumbers = false;
     function<void(string)> exitAction;
     function<void(string)> keyAction;
 };
@@ -95,6 +102,9 @@ void TextField::setText(vector<string> text) {
 vector<string> TextField::getText() {
     return text;
 }
+void TextField::setLineNumbers(bool lineNumbers) {
+    this->lineNumbers = lineNumbers;
+}
 void TextField::setExitAction(function<void(string)> exitAction) {
     this->exitAction = exitAction;
 }
@@ -107,6 +117,8 @@ void TextField::paint(int x, int y, int width, int height) {
     WindowElement::paint(x, y, width, height);
     if (hidden) return;
 
+    int numberOffset = lineNumbers ? 4 : 0;
+
     drawBox(x, y, width, height, state);
 
     if (selected) {
@@ -116,10 +128,18 @@ void TextField::paint(int x, int y, int width, int height) {
     }
     if (text.size() > 0) {
         for (int i = 0; i < text.size(); i++) {
-            drawText((selected && selectedText.first == i ? "_" : "") + text[i], x + 2, y + 1 + i, selected && selectedText.first == i ? CYAN : DIM);
+            if (lineNumbers) {
+                drawText(to_string(i + 1), x + 1, y + 1 + i, DIM);
+                drawText("|", x + numberOffset - 1, y + 1 + i, BLACK);
+            }
+            drawText(text[i], x + numberOffset, y + 1 + i, GRAY);
+
+            printf("\033[48;5;45m");
+            setCursorPosition(x + numberOffset + selectedText.second, y + 1 + selectedText.first);
+            printf("\033[7m \033[0m");
         }
     } else {
-        drawText((selected ? "_" : "") + templateText, x + 2, y + 1, DIM);
+        drawText((selected ? "_" : "") + templateText, x + numberOffset, y + 1, DIM);
     }
 }
 
